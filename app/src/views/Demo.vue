dde439834b183f6bdab21cd27c1c45e8af8f0fcc
liujingxue <523624469@qq.com>
2017-07-12 16:16:57 +0800

    修改了Demo.vue

---
+++ app/src/views/Demo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    这是Demo页面,2017
+  </div>
+</template>
+<style>
+
+</style>
+<script>
+  export default {
+    data(){
+      return{
+        msg: 'hello vue'
+      }
+    }
+  }
+</script>
